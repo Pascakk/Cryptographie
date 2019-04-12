b7f933096a8ef23b892dd5b54ddb963343c33d88
Unknown <pascal.beroule@gmail.com>
2019-04-12 10:12:57 +0200

    Optimisation et Simplification de l'interface [Vignère]

--- Wignère.py
+++ Wignère.py
@@ -3,16 +3,12 @@ from fractions import gcd
 from functools import reduce
 from tqdm import tqdm, trange
 from collections import Counter # Recherche de caractère le plus fréquent
-# n° du fichier à décoder
-num = 6
-# caractère supposé le plus utilisé (e, a ou l'espace en francais)
-ref = ord(" ")
 
-
-with open("message" + str(num) + ".txt", encoding="utf8") as file:
-    message = file.read()
-
-tailleMessage = len(message)
+def ouvrir_message(num):
+    '''Ouvre le message correspondant au fichier donné en entrée'''
+    with open("message" + str(num) + ".txt", encoding="utf8") as file:
+        contenu = file.read()
+    return contenu
 
 def plusFrequent(D):
     '''prend un dictionnaire et renvoie la clé dont l'indice est le plus grand'''
@@ -22,7 +18,6 @@ def plusFrequent(D):
         if D[i] > maxi:
             maxi = D[i]
             car = i
-    print (car)
     return car
 
 def find_gcd(list):
@@ -58,36 +53,43 @@ def taille_cle(length, nbRepetitions, nbSequences):
                     """
     return Counter(taillesPossibles).most_common()[0][0] # Renvoie la taille de la clé (valeur possible la plus fréquente)
 
-tailleCle = taille_cle(4, 3, 4) # taille de la séquence, nombre de répétitions, nombre de séquence vérifiant ces critères
-print(tailleCle)
+def find_cle(tailleCle):
+    # Séparation du texte en n morceaux correspondants aux caractères encodés par le même indice de la clé
+    T = []
+    for i in range(tailleCle):
+        j = 0
+        T.append([])
+        for j in range(floor(tailleMessage/tailleCle)):
+            T[i].append(message[i+j*tailleCle])
+    
+    #décryptage de la clé par analyse fréquentielle
+    cle = []
+    for i in range(tailleCle):
+        occurences = {}
+        for j in T[i]: #recherche du caractère le plus fréquent
+            if j in occurences:
+                occurences[j]+=1
+            else :
+                occurences[j] = 1
+        cle.append(ord(plusFrequent(occurences))-ref)
+    return cle
+        
+def decrypte(cle):
+    # Décryptage du message
+    tailleCle = len(cle)
+    clair = ""
+    indice = 0
+    for i in range(tailleMessage):
+        clair += chr(ord(message[i]) - cle[i%tailleCle])
+    return clair
 
-# Séparation du texte en n morceaux correspondants aux caractères encodés par le même indice de la clé
-T = []
-for i in range(tailleCle):
-    j = 0
-    T.append([])
-    for j in range(floor(tailleMessage/tailleCle)):
-        T[i].append(message[i+j*tailleCle])
-print(T)
+'''partie interface : '''
 
-#décryptage de la clé par analyse fréquentielle
-cle = []
-for i in range(tailleCle):
-    occurences = {}
-    for j in T[i]: #recherche du caractère le plus fréquent
-        if j in occurences:
-            occurences[j]+=1
-        else :
-            occurences[j] = 1
-    print(occurences)
-    cle.append(ord(plusFrequent(occurences))-ref)
-
-# Décryptage du message
-clair = ""
-indice = 0
-for i in range(tailleMessage):
-    clair += chr(ord(message[i]) - cle[i%tailleCle])
-
-# Affichage
-print(clair)
-print(cle)
+# n° du message à ouvrir
+message = ouvrir_message(2) # Variable globale contenant le message crypté
+tailleMessage = len(message) # Variable globale contenant la taille du message
+ref = ord(" ") # caractère supposé le plus utilisé (e, a ou l'espace en francais)
+# Affichage des résultats
+cle = find_cle(taille_cle(4, 3, 4)) # taille de la séquence, nombre de répétitions, nombre de séquences vérifiant ces critères
+print(decrypte(cle)) # Message en clair
+print(cle) # cle correcpondant au message
